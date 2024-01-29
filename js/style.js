c4b92f6cbca933d2439d2261e63e7ccb5edd7195
Bolade Olalekan <lekanseyibolade@gmail.com>
2024-01-29 13:39:31 +0100

    Completed desktop view

--- js/style.js
+++ js/style.js
@@ -4,15 +4,16 @@ let success_msg = document.querySelector('.successMsg');
 let form_input = document.querySelector('form');
 let submit_btn = document.querySelector('.submit-btn');
 let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-let subscribePage = document.querySelector('.container-1');
+let subscribePage = document.querySelector('.cont-bcg');
 let successPage = document.querySelector('.container-2');
+let showSuccess = document.querySelector('.hide');
 
 
 form_input.addEventListener("submit" , (e) => {
     e.preventDefault();
 
-    subscribePage.classList.add("hide");
-    successPage.classList.remove("hide");
+    subscribePage.style.display = "none";
+    showSuccess.style.display = "block";
 
     email_input = email_input.value;
     success_msg.innerHTML = `A confirmation email has been sent to <strong> ${email_input} </strong>. Please open it and click the button inside to confirm your subscription.`;
